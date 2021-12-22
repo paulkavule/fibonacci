7e7bda20a9bf8c1917b9aa3cd88d1b29206c26dd
Paul Kavule <paul@Pauls-MacBook-Pro.local>
2021-12-22 20:50:31 +0300

    adding head recussion

--- head/fib.go
+++ head/fib.go
@@ -1,9 +1,18 @@
 package head
 
-func Fibonacci(num int) int {
-	return fib(num, 0, 1)
+import "errors"
+
+func Fibonacci(num int) (int, error) {
+	if num <= 0 {
+		return 0, errors.New("input has to be greater than zero")
+	}
+	fib := fib(num)
+	return fib, nil
 }
 
-func fib(num int, next int, acc int) int {
-	return 0
+func fib(num int) int {
+	if num <= 1 {
+		return num
+	}
+	return fib(num-1) + fib(num-2)
 }
