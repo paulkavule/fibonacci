32935c5f99f66c59cec53e365a95172f8396af00
Paul Kavule <paul@Pauls-MacBook-Pro.local>
2021-12-22 20:26:01 +0300

    adding zero check

--- tail/fib.go
+++ tail/fib.go
@@ -1,7 +1,13 @@
 package tail
 
-func Fibonacci(num int) int {
-	return num
+import "errors"
+
+func Fibonacci(num int) (int, error) {
+	if num <= 0 {
+		return 0, errors.New("number should be greater than 0")
+	}
+	fib := fib(num, 0, 1)
+	return fib, nil
 }
 
 func fib(num int, next int, result int) int {
